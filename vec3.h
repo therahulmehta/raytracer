e5f075a0d82c09a0429685f5f087915aaf21e282
Rahul Mehta <rahul@tests-MacBook-Pro.local>
2021-12-14 10:23:31 -0500

    inital commit, setup raytracer project

---
+++ vec3.h
@@ -0,0 +1,18 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath> 
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    public: 
+    // define operators
+}
+
+// aliases for clarity 
+using point3 = vec3; 
+using color = vec3;
+
+#endif
