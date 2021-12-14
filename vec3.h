f8f21055ba3fd3e4bf7f4843a6c5fa78679677b2
Rahul Mehta <rahul@tests-MacBook-Pro.local>
2021-12-14 11:07:23 -0500

    create header file for VEC3 variables and functions

--- vec3.h
+++ vec3.h
@@ -4,11 +4,50 @@
 #include <cmath> 
 #include <iostream>
 
-using std::sqrt;
-
 class vec3 {
     public: 
-    // define operators
+    // constructor
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    // return coordinate
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    // operators 
+
+    vec3 operator-() const{ return vec3(-e[0], -e[1], -e[2]); }
+    double operator[] (int i) const { return  e[i]; }
+    double& operator[] (int i) { return e[i]; }
+
+    vec3& operator+=(const vec3 &v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this; 
+    }
+
+    vec3& operator*=(const double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec& operator/=(const double t) {
+        return *this *= 1/t;
+    }
+
+    double length() const{
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const{
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    double e[3];
 }
 
 // aliases for clarity 
